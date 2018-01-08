49c460fb4232593cb6a50e0f6036f1c4c6eff810
Cara Lang <caravlang@gmail.com>
2018-01-08 17:58:46 -0500

    created Order and LineItem models

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,5 +1,8 @@
 const User = require('./user')
 const Baby = require('./baby')
+const LineItem = require('./lineItem')
+const Order = require('./order')
+
 /**
  * If we had any associations to make, this would be a great place to put them!
  * ex. if we had another model called BlogPost, we might say:
@@ -15,7 +18,9 @@ const Baby = require('./baby')
  */
 module.exports = {
   User,
-  Baby
+  Baby,
+  LineItem,
+  Order
 }
 
 // completed column in orders
