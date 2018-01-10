16eab831ed8432788550ab533f45fdb6fc9c2ec6
Sierra Saitta-Zelterman <ssaitta13@gmail.com>
2018-01-10 14:39:03 -0500

    feature: finished adding all tests for api/babies

--- server/api/babies.spec.js
+++ server/api/babies.spec.js
@@ -33,15 +33,16 @@ describe('Baby routes', () => {
                     inventory_qty: '20',
                     imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/71E25QZTEVL.gif',
                     category: 'common'
-            }),
-                Baby.create({
-                    name: 'Mystic',
-                    poem: 'Once upon a time in a land far away, A baby unicorn was born one day in May, Keep Mystic with you she\'s a prize, You\'ll see the magic in her blue eyes.',
-                    price: '5000.00',
-                    inventory_qty: '1',
-                    imageUrl: 'http://www.tycollector.com/beanies/bb-images/bernie.jpg',
-                    category: 'rare'
             })
+            // ,
+            //     Baby.create({
+            //         name: 'Mystic',
+            //         poem: 'Once upon a time in a land far away, A baby unicorn was born one day in May, Keep Mystic with you she\'s a prize, You\'ll see the magic in her blue eyes.',
+            //         price: '5000.00',
+            //         inventory_qty: '1',
+            //         imageUrl: 'http://www.tycollector.com/beanies/bb-images/bernie.jpg',
+            //         category: 'rare'
+            // })
         })
 
         it('GET /api/babies', () => {
@@ -68,6 +69,23 @@ describe('Baby routes', () => {
             return request(app)
                 .post('/api/babies')
                 .send({
+                    name: 'Mystic',
+                    poem: 'Once upon a time in a land far away, A baby unicorn was born one day in May, Keep Mystic with you she\'s a prize, You\'ll see the magic in her blue eyes.',
+                    price: '5000.00',
+                    inventory_qty: '1',
+                    imageUrl: 'http://www.tycollector.com/beanies/bb-images/bernie.jpg',
+                    category: 'unicorn'
+                })
+                .expect(201)
+                .then(res => {
+                    expect(res.body.name).to.be.equal('Mystic')
+                })
+        })
+
+        it('POST /api/babies', () => {
+            return request(app)
+                .post('/api/babies')
+                .send({
                     name: babyName1,
                     poem: 'This little dog can\'t wait to grow, To rescue people lost in the snow, Don\'t let him out-keep him on your shelf, He doesn\'t know how to rescue himself!',
                     price: '1000.00',
@@ -80,6 +98,7 @@ describe('Baby routes', () => {
                     expect(res.text).to.be.equal('That Baby is already home')
                 })
         })
+
 
         it('PUT /api/babies', () => {
             return request(app)
@@ -95,16 +114,17 @@ describe('Baby routes', () => {
                 .expect(201)
                 .expect('Content-Type', /json/)
                 .expect(function (res) {
-                    expect(res.body).to.eqlal({
-                        name: 'Mystic',
-                        poem: 'Once upon a time in a land far away, A baby unicorn was born one day in May, Keep Mystic with you she\'s a prize, You\'ll see the magic in her blue eyes.',
-                        price: '5000.00',
-                        inventory_qty: '500',
-                        imageUrl: 'http://www.tycollector.com/beanies/bb-images/bernie.jpg',
-                        category: 'rare'
-                    });
+                    console.log(res)
+                    expect(res.body.category).to.equal('rare');
+                    expect(res.body.inventory_qty).to.equal(500)
                 })
             //we need to create a 201 status created for our posts
         }) // end describe('/api/babies')
+
+        it('DELETE /api/babies', () => {
+            return request(app)
+                .delete('/api/babies/3')
+                .expect(204)
+        })
     })
 })
