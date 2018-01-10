a22288e69a47dc5ddec2f87c9905a54dfb36267e
Sierra Saitta-Zelterman <ssaitta13@gmail.com>
2018-01-10 17:26:53 -0500

    Carousel done on Home page, only showing when not logged in and on landing page

--- client/components/main.js
+++ client/components/main.js
@@ -33,7 +33,9 @@ const Main = (props) => {
         }
       </nav>
       {children} {/* this is where the email and password login form is currently located*/}
-      <Home />
+      {!isLoggedIn
+        ? <Home />
+        : ''}
     </div>
   )
 }
