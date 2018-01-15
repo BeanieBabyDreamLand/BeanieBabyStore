ee1f42dca63732ae8fcead54365850a9881c6b34
Cara Lang <caravlang@gmail.com>
2018-01-15 13:47:13 -0500

    FEATURE: (post review) add review form rendering, still need to add back end functionality

--- client/components/review.js
+++ client/components/review.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {withRouter, Link} from 'react-router-dom'
-import store, {fetchBabies, babiesThunk, fetchOneBaby, getBabyCategory, getSearchResults, fetchAllUsers} from '../store'
+import store, {fetchBabies, babiesThunk, fetchOneBaby, getBabyCategory, getSearchResults, fetchAllUsers, postReview} from '../store'
 
 function mapStateProps(state){
   return {
@@ -14,16 +14,32 @@ function mapDispatchProps(dispatch){
         loadData (){
             dispatch(fetchAllUsers())
             dispatch(fetchOneBaby(this.req.params.id))
+        },
+        handleReviewSubmit (evt) {
+            evt.preventDefault()
+            const rating = evt.target.rating.value
+            const text = evt.target.description.value
+            const userId = evt.target.userId.value
+            const babyId = evt.target.babyId.value
+            dispatch(postReview({rating, text, babyId, userId}))
         }
 
     }
 }
 
 export const review = (props) => {
-    console.log('!!!!',props.babies, props.match.params)
-    let users = props.users
-    console.log(users)
+
+
+    const {isLoggedIn} = props
+    console.log('props', props.babies, props.match.params)
+    // let users = props.users
+    // console.log('users', users)
     let thisBaby = props.babies
+    let user = props.user.id
+    let baby = thisBaby.id
+    console.log('BABYID: ', thisBaby.id)
+    console.log('USERID: ', props.user.id)
+
     return (
         <div>
         <h3>Reviews and high praise for {thisBaby.name}</h3>
@@ -39,7 +55,45 @@ export const review = (props) => {
                 )
             })}</div>
         }
+
+        {/* add review form */}
+
+        <div>
+        {
+          isLoggedIn
+            ? <div>
+              {/* the message will render if user is not logged in */}
+              <h1>Please Log In to Add A Review</h1>
+            </div>
+            : <div >
+              {/* The form will render if user is logged in */}
+              <h3>Add Your Own Review for {thisBaby.name}</h3>
+                <form onSubmit={props.handleReviewSubmit} name="newReview">
+                    <h4>Rating: </h4><input
+                        name="rating"
+                        type="text"
+                        placeholder="Enter A Rating Between 1 and 5" />
+                    <h4>Description: </h4><input
+                        name="description"
+                        type="text"
+                        placeholder="Enter Your Review" />
+                    <input
+                        name="userId"
+                        type="text"
+                        value={user} />
+                    <input
+                        name="babyId"
+                        type="text"
+                        value={baby} />
+                    <button type="submit">Add Review</button>
+                </form>
+            </div>
+        }
         </div>
+
+        </div>
+
+
     )
 }
 
